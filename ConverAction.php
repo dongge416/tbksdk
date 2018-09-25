7b83f3036be01429bd2d95cedd1bb9963ceed871
dongge416 <dongge416@gmail.com>
2018-09-25 09:52:02 +0800

    添加converAction接口

---
+++ ConverAction.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers:x-requested-with,content-type');   
+header("content-type:text/html;charset=utf-8");
+date_default_timezone_set('Asia/Shanghai');
+
+include "ApiUtils.php";
+include "RStringUtil.php";
+
+$content_str = $_GET['content'];
+$result_data = ApiUtils::convertApi($content_str);
+echo $result_data;
+
+?>
